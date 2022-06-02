e4a1d0d123f2737099773192f000704eb644a27d
nicole-fer <nicolefernandes@alu.ufc.br>
2022-06-02 15:53:22 -0300

    Camada Request

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,13 +13,39 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-// primeiro parametro é a barra da rota e a segunda é a função
-Route::get('/hello-world', function () {
-    return 'Olá Mundo!!';
+
+
+
+Route::get('user/{user}', function(\App\Models\User $user){
+    return $user;
+});
+
+
+// Agrupamento de Rotas
+
+Route::prefix('usuarios') ->group(function(){
+    Route::get('', function(){
+        return 'usuário';
+    })->name('usuarios');
+
+    Route::get('/{id}', function(){
+        return 'Mostrar Detalhes';
+    })->name('usuarios.show');
+
+    Route::get('/{id}/tags', function(){
+        return 'Tags do Usuário';
+    })->name('usuarios.tags');
+});
+
+///{string?} se torna opcional
+Route::get('/a-empresa/{string?}', function ($string = null) {
+    return $string;
    /*  return view('welcome'); */
 });
 
-Route::get('/', function () {
-    return 'deu certo';
+// primeiro parametro é a barra da rota e a segunda é a função
+Route::get('/users/{paramA}/{paramB}', function ($paramA,$paramB) {
+    return $paramA . ' - ' . $paramB;
    /*  return view('welcome'); */
 });
+
