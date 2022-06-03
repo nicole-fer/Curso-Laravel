eadd5770bed26a5bafe5ac4549bbef59dec9141b
nicole-fer <nicolefernandes@alu.ufc.br>
2022-06-03 14:40:35 -0300

    Salvando Registros

--- app/Http/Controllers/BusinessController.php
+++ app/Http/Controllers/BusinessController.php
@@ -9,10 +9,18 @@ class BusinessController extends Controller
 {
     public function index()
     {
-        $businesses = Business::all();
-        $businesses = Business::find(1);
+        $businesses = Business::create([
+            'name' => 'Jon Snow',
+            'email' => 'jon@snow.com',
+            'adress' => 'Rua a quadra B'
+        ]);
+
+        dd($businesses);
+
+        //$businesses = Business::all();
+       // $businesses = Business::find(1);
        // $businessesWhere = Business::where('name', 'Runte-Wunsch')->get();
-        dd($businesses, $businesses);
+       // dd($businesses, $businesses);
        // dd($businessesWhere);
 
        //$businessesWhereFirst -> encontra o primeiro que encontrar, caso tiverem mais de uma pessoas com o mesmo nome
